6ae3001b6edc12ce421bce232f1980a26bf82843
mori <mikomori0909@gmail.com>
2018-06-01 07:49:52 +0800

    refactoring

--- src/RecipeInput.js
+++ src/RecipeInput.js
@@ -8,15 +8,25 @@ class RecipeInput extends Component {
             image:'',
             name:'',
             ingredients:[''],
-            instruction: ''
+            instructions: ''
         }
         this.addIngredient = this.addIngredient.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleIng = this.handleIng.bind(this)
     }
 
     addIngredient(){
         this.setState((prevState, props) => {return {ingredients: [...prevState.ingredients, '']}})
     }
-
+    handleChange(e){
+        this.setState({[e.target.name]:e.target.value})
+    }
+    handleIng(e){
+        const newIng = this.state.ingredients.slice();
+        newIng[e.target.id] = e.target.value
+        this.setState({ingredients: newIng})
+    }
+    
     render() {
         return (
         <div className={`wrapper ${this.props.class}`}>
@@ -32,28 +42,24 @@ class RecipeInput extends Component {
                 }}>
 
                     <p>recipe name</p>
-                    <input type="text" value={this.state.name} onChange={e =>{
-                        this.setState({name:e.target.value}, ()=>console.log(this.state.name))
-                    }}/>
+                    <input type="text" value={this.state.name} name='name'
+                    onChange={this.handleChange}/>
 
                     <p>ingredients</p>
                     {this.state.ingredients.map((ing,idx)=> 
-                        <input type="text" key={idx} value={this.state.ingredients[idx]} style={{marginBottom:'10px'}}
-                        onChange={e =>{
-                            const newIng = this.state.ingredients.slice();
-                            newIng[idx] = e.target.value
-                            this.setState({ingredients: newIng},()=>console.log(this.state.ingredients))
-                        }}/>
+                        <input type="text" key={idx} id={idx} value={this.state.ingredients[idx]} style={{marginBottom:'10px'}}
+                        onChange={this.handleIng}/>
                     )}                       
+
                     <div id='add' onClick={this.addIngredient}>+</div>
 
                     <p>instructions</p>
-                    <textarea type="text" value={this.state.instructions} onChange={e =>{
-                        this.setState({instructions:e.target.value}, ()=>console.log(this.state.instructions))
-                    }}></textarea>
+                    <textarea type="text" value={this.state.instructions} name='instructions'
+                    onChange={this.handleChange}>
+                    </textarea>
 
                     <p>image link</p>
-                    <input type="text" value={this.state.image} onChange={e =>this.setState({image: e.target.value})}/>
+                    <input type="text" value={this.state.image} name='image' onChange={this.handleChange}/>
 
                     <input id='submit-btn' type="submit"/>
 
