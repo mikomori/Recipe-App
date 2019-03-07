928539573c10cf459b0af4cb9396c2b2ac362af8
mori <mikomori0909@gmail.com>
2019-03-08 00:09:03 +0800

    fix styling

--- src/Recipe.js
+++ src/Recipe.js
@@ -13,11 +13,11 @@ const Recipe = props =>
                 <h1>{props.name}</h1>
                 {/* <div className="line"></div> */}
 
-                <p className='title'>ingredients</p>
-                {props.ingredients.map((ing,idx)=> <p key={idx} style={{fontSize:'14px', margin:'5px 0 0', letterSpacing:'0'}}>{ing}</p>)}
+                <p className='title'>INGREDIENTS</p>
+                {props.ingredients.map((ing,idx)=> <p key={idx} style={{margin:'5px 0 0', letterSpacing:'0'}}>{ing}</p>)}
 
-                <p className='title' style={{marginTop:'30px'}}>instructions</p>
-                <p style={{fontSize:'14px', letterSpacing:'0', lineHeight:'20px'}}>{props.instructions}</p>
+                <p className='title' style={{marginTop:'30px'}}>INSTRUCTIONS</p>
+                <p style={{letterSpacing:'0', lineHeight:'20px'}}>{props.instructions}</p>
             </div>
         </div>
         
